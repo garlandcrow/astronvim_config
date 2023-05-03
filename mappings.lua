48785677837d3b7b66ed2ab8cea9f360085f3747
garlandcrow <garlandcrow@proton.me>
2023-05-03 22:33:27 +0900

    add theme and key remaps

--- mappings.lua
+++ mappings.lua
@@ -8,10 +8,16 @@ return {
   n = {
     -- second key is the lefthand side of the map
     -- mappings seen under group name "Buffer"
+    ["i"] = { "k" },
+    ["k"] = { "j" },
+    ["j"] = { "h" },
+    ["h"] = { "i" },
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<leader>bD"] = {
       function()
-        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(bufnr) end)
+        require("astronvim.utils.status").heirline.buffer_picker(
+          function(bufnr) require("astronvim.utils.buffer").close(bufnr) end
+        )
       end,
       desc = "Pick to close",
     },
@@ -20,6 +26,12 @@ return {
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+  },
+  v = {
+    ["i"] = { "k" },
+    ["k"] = { "j" },
+    ["j"] = { "h" },
+    ["h"] = { "i" },
   },
   t = {
     -- setting a mapping to false will disable it
